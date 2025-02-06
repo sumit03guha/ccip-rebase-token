5a3206f33da3bfee9152bd69de5a37f8da9b77a7
Sumit Guha <sumit03guha@gmail.com>
2025-02-07 01:01:34 +0530

    feat: add INTEREST_RATE constant to Constants.sol

--- src/Constants.sol
+++ src/Constants.sol
@@ -4,3 +4,4 @@ pragma solidity 0.8.28;
 
 uint256 constant PRECISION_FACTOR = 1e18;
 bytes32 constant MINTER_AND_BURNER_ROLE = keccak256("MINTER_AND_BURNER_ROLE");
+uint256 constant INTEREST_RATE = 5e10;
